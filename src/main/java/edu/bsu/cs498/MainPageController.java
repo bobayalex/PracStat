276027aa0127a2c176bf60d3696ccd96e13e1d85
Dexter Edwards <dedwardscs222@gmail.com>
2019-03-20 16:29:38 -0400

    Add spinners to gridpane

--- src/main/java/edu/bsu/cs498/MainPageController.java
+++ src/main/java/edu/bsu/cs498/MainPageController.java
@@ -2,24 +2,62 @@ package edu.bsu.cs498;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Menu;
+import javafx.scene.Node;
 import javafx.scene.control.MenuBar;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
+import javafx.scene.control.Spinner;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
-    @FXML private AnchorPane anchorPane;
     @FXML private MenuBar menuBar;
+    @FXML private GridPane statGrid;
+    @FXML private List<Spinner<Integer>> statSpinners = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setUpMenuBar();
+//        setUpMenuBar();
+        setUpGridPane();
     }
 
-    private void setUpMenuBar() {
+    private void printSpinnerVals() {
+        for (Spinner<Integer> spinner: statSpinners) {
+            System.out.println("Value is " + spinner.getValue());
+        }
+    }
+
+    private void printNodeFromGridPane(){
+        Spinner<?> spinner = getSpinner(0,1);
+        int foo = (Integer) spinner.getValue();
+        System.out.println("Value is " + foo);
+    }
+
+    private void setUpGridPane() {
+        for(int i = 0; i < 12; i++){
+            for(int j = 0; j < 16; j++){
+                Spinner<Integer> spinner = new Spinner<>(0, 10000, 0, 1);
+                spinner.setPrefWidth(100);
+                spinner.setPrefHeight(30);
+                statSpinners.add(spinner);
+                statGrid.add(spinner,i,j);
+            }
+        }
 
     }
+
+    private Spinner<Integer> getSpinner(int row, int col){
+        for(Node child: statGrid.getChildren()){
+            if(GridPane.getColumnIndex(child) != null && GridPane.getRowIndex(child) != null && GridPane.getColumnIndex(child) == col && GridPane.getRowIndex(child) == row){
+                return (Spinner<Integer>)child;
+            }
+        }
+        return null;
+    }
+
+//    private void setUpMenuBar() {
+//
+//    }
 }
