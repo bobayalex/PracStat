9d6968f0f4164cedc6998d632118d27474d192ef
Adam Wessel <15wessea@gmail.com>
2019-03-20 16:26:15 -0400

    Utilize a grammar and multiple threads to help boost voice recognition accuracy and speed

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -7,46 +7,80 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
+/*
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 575, 500));
         primaryStage.show();
-
-
-
+*/
         Configuration configuration = new Configuration();
 
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         configuration.setDictionaryPath("resource:/fxml/pracstatdict.dic"); //"resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict"
         configuration.setLanguageModelPath("resource:/fxml/pracstatlangm.lm"); //"resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin"
 
+        //configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        //configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+
         LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
         // Start recognition process pruning previously cached data.
         recognizer.startRecognition(true);
-        System.out.println("started recognition");
-        SpeechResult result = recognizer.getResult();
+        /*
+        while(true) {
+            System.out.println("started recognizing...");
+            System.out.println("say: seven block, twelve ace, etc.");
+
+            String utterance = recognizer.getResult().getHypothesis();
+
+            if (utterance.startsWith("exit"))
+                break;
+            if (utterance.startsWith("seven")) {
+                recognizer.stopRecognition();
+                System.out.println("recognized that utterance started with BLOCK!!!");
+                recognizer.startRecognition(true);
+            }
+        }
+        */
+        //System.out.println("started recognition");
+        while(true){
+
+            SpeechResult result = recognizer.getResult();
+
+            System.out.println(result.getHypothesis());
+
+        }
+        //SpeechResult result;
+
+
         // Pause recognition process. It can be resumed then with startRecognition(false).
         //recognizer.stopRecognition();
 
-
+/*
         while ((result = recognizer.getResult()) != null) {
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
         }
         recognizer.stopRecognition();
         System.out.println("stopped recognition");
-
+*/
 
     }
 
