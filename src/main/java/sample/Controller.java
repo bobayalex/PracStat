b01f3d303666d6166da850cb1c0e796f23a33c0d
Adam Wessel <15wessea@gmail.com>
2019-03-24 20:37:36 -0400

    Add JavaFX Start Voice Recognition button to toggle voice recognizer.

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
+import model.SpeechRecognizerMain;
+
 public class Controller {
-    String test_string = "this string";
+
+    private boolean firstRun = true;
+    private boolean recognitionStarted = false;
+    private SpeechRecognizerMain mySpeechRecognizer;
+
+    @FXML
+    private void handleButtonAction() {
+        // button was clicked.. do something
+        if(firstRun = true) {
+            mySpeechRecognizer = new SpeechRecognizerMain();
+            mySpeechRecognizer.SpeechRecognizerMain();
+            firstRun = false;
+            recognitionStarted = true;
+        }
+        else if(firstRun = false) {
+            if(recognitionStarted = true) {
+                mySpeechRecognizer.ignoreSpeechRecognitionResults();
+            }
+            else if(recognitionStarted = false) {
+                mySpeechRecognizer.stopIgnoreSpeechRecognitionResults();
+            }
+        }
+    }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -25,25 +25,24 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-/*
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 575, 500));
         primaryStage.show();
-*/
-        Configuration configuration = new Configuration();
 
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("resource:/fxml/pracstatdict.dic"); //"resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict"
-        configuration.setLanguageModelPath("resource:/fxml/pracstatlangm.lm"); //"resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin"
+        //Configuration configuration = new Configuration();
+
+        //configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        //configuration.setDictionaryPath("resource:/fxml/pracstatdict.dic"); //"resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict"
+        //configuration.setLanguageModelPath("resource:/fxml/pracstatlangm.lm"); //"resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin"
 
         //configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
         //configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
 
-        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+        //LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
         // Start recognition process pruning previously cached data.
-        recognizer.startRecognition(true);
+        //recognizer.startRecognition(true);
         /*
         while(true) {
             System.out.println("started recognizing...");
@@ -59,7 +58,7 @@ public class Main extends Application {
                 recognizer.startRecognition(true);
             }
         }
-        */
+
         //System.out.println("started recognition");
         while(true){
 
@@ -74,15 +73,16 @@ public class Main extends Application {
         // Pause recognition process. It can be resumed then with startRecognition(false).
         //recognizer.stopRecognition();
 
-/*
+
         while ((result = recognizer.getResult()) != null) {
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
         }
         recognizer.stopRecognition();
         System.out.println("stopped recognition");
-*/
 
+*/
     }
+
 
 
     public static void main(String[] args) {
