13ce8a29fe2f4884a00697c355a9a2838cab616b
Adam Wessel <15wessea@gmail.com>
2019-03-27 16:39:48 -0400

    Add status label for specific conditions: "loading, start to speak, ignoring, listening.."

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import model.SpeechRecognizerMain;
 
 public class Controller {
@@ -9,23 +10,26 @@ public class Controller {
     private boolean firstRun = true;
     private SpeechRecognizerMain mySpeechRecognizer;
 
-    @FXML
-    Button speechRecBtn;
+    @FXML Button speechRecBtn;
+    @FXML Label statusLabel;
 
     @FXML
-    private void handleButtonAction() {
+    private void handleButtonAction(){
         if(firstRun) {
+            statusLabel.setText("Loading Speech Recognizer...");
             mySpeechRecognizer = new SpeechRecognizerMain();
             mySpeechRecognizer.SpeechRecognizerMain();
             firstRun = false;
             speechRecBtn.setStyle("-fx-background-color: Green");
             speechRecBtn.setText("Speech Recognition");
+            statusLabel.setText("You can start to speak...");
         }
         else if(!firstRun) {
             if(!mySpeechRecognizer.getIgnoreSpeechRecognitionResults()) {
                 mySpeechRecognizer.ignoreSpeechRecognitionResults();
                 System.out.println("ignoring speech recognition results");
                 speechRecBtn.setStyle("-fx-background-color: Red");
+                statusLabel.setText("Ignoring speech recognition results...");
                 //speechRecBtn.setBackground(javafx.scene.paint.Color.color(green));
                 //speechRecBtn.setBackground(Color.red);
             }
@@ -34,6 +38,7 @@ public class Controller {
                 System.out.println("listening to speech recognition results");
                 //speechRecBtn.setBackground(Color.green);
                 speechRecBtn.setStyle("-fx-background-color: Green");
+                statusLabel.setText("Listening to speech recognition results...");
             }
         }
     }
