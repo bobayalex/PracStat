af0955adc8237d3c65134294c24cb220aa3f8022
Adam Wessel <15wessea@gmail.com>
2019-03-27 20:14:14 -0400

    Incorporate a sound that plays when voice recognition is on

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -3,18 +3,26 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import model.SpeechRecognizerMain;
+
+import java.io.File;
 
 public class Controller {
 
     private boolean firstRun = true;
     private SpeechRecognizerMain mySpeechRecognizer;
+    private String soundFile = "chime.mp3";
+    private MediaPlayer mediaPlayer;
 
     @FXML Button speechRecBtn;
     @FXML Label statusLabel;
 
     @FXML
     private void handleButtonAction(){
+        Media sound = new Media(new File(soundFile).toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
         if(firstRun) {
             statusLabel.setText("Loading Speech Recognizer...");
             mySpeechRecognizer = new SpeechRecognizerMain();
@@ -23,6 +31,7 @@ public class Controller {
             speechRecBtn.setStyle("-fx-background-color: Green");
             speechRecBtn.setText("Speech Recognition");
             statusLabel.setText("You can start to speak...");
+            mediaPlayer.play();
         }
         else if(!firstRun) {
             if(!mySpeechRecognizer.getIgnoreSpeechRecognitionResults()) {
@@ -39,6 +48,7 @@ public class Controller {
                 //speechRecBtn.setBackground(Color.green);
                 speechRecBtn.setStyle("-fx-background-color: Green");
                 statusLabel.setText("Listening to speech recognition results...");
+                mediaPlayer.play();
             }
         }
     }
