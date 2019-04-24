caa2e8ed8c676e5dc411c657f1a58744bd144fef
Dexter Edwards <dedwardscs222@gmail.com>
2019-04-24 17:19:09 -0400

    Fix stuff again

--- src/main/java/edu/bsu/cs498/Main.java
+++ src/main/java/edu/bsu/cs498/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     // combine branches
     // clean up code/beautify fxml pages
 
-//    private static MainPageController mainPageController;
+    private static MainPageController mainPageController;
     @Override
     public void start(Stage primaryStage) throws Exception{
         String fxmlPath = "/fxml/startPage.fxml";
@@ -29,8 +29,8 @@ public class Main extends Application {
         primaryStage.setMaximized(true);
         primaryStage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::closeWindowAction);
         primaryStage.show();
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainPage.fxml"));
-//        mainPageController = loader.getController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainPage.fxml"));
+        mainPageController = loader.getController();
     }
 
     private void closeWindowAction(WindowEvent windowEvent) {
